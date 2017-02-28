d3855753f8d1b83b7d68cedd13dffa3aae4ca206
al3Co <aldocontrego@gmail.com>
2017-02-28 22:48:13 +0100

    Send Data
    Send Data to Arduino by using serial port

--- Serial_Read/pruebaArd.py
+++ Serial_Read/pruebaArd.py
@@ -1,3 +1,8 @@
+#!/usr/bin/python
+"""
+    Connect to /dev/tty.usbmodem1421 wuth 9600 bps
+"""
+
 import serial
 import time
 
@@ -5,9 +10,13 @@ sender = '1'
 sender2 = '2'
 
 s = serial.Serial(port='/dev/tty.usbmodem1421', baudrate=9600)
-#time.sleep(3)
-#s.write(sender)
-time.sleep(2)
-s.write(sender2)
+try:
+    time.sleep(3)
+    s.write(sender)
+    time.sleep(2)
+    s.write(sender2)
+    #break
+except ValueError:
+    print (ValueError)
 #s.read()
 #s.readline()
